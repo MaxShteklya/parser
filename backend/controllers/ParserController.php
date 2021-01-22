7f41cd51a19405c07330ca506caa6187b9ae78ab
Your Name <you@example.com>
2021-01-23 00:17:16 +0200

    Parsee beginning

---
+++ backend/controllers/ParserController.php
@@ -0,0 +1,72 @@
+<?php
+namespace backend\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use common\models\LoginForm;
+use backend\models\Parser;
+
+/**
+ * Parser controller
+ */
+class ParserController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'actions' => ['login', 'error'],
+                        'allow' => true,
+                    ],
+                    [
+                        'actions' => ['logout', 'index', 'parser'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        //exit("HERE");
+        return $this->render('index');
+    }
+
+    public function actionParser(){
+        //exit("HERE");
+        return $this->render('parser');
+    }
+}
---
+++ common/models/Parser.php
@@ -0,0 +1,14 @@
+<?php
+namespace common\models;
+
+use Yii;
+use yii\base\Model;
+use yii\db\ActiveRecord;
+
+/**
+ * Parser
+ */
+class Parser extends Model
+{
+
+}
